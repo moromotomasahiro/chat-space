a5a22cb4eaa296a1717545e7cc307285325b69d6
moromotomasahiro <moromotomasahiro@icloud.com>
2019-12-03 16:34:27 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -6,21 +6,21 @@ $(function(){
           img = `<img src="${message.image.url}">`
       }
       var html =
-               `<div class="contents_main1"data-message-id=${message.id}>
-                  <div class="message__sender1">
-                     ${message.user_name}
-                  </div>
-                  <div class="message__date1">
-                     ${message.data}
-                  </div>
-                  <div class="message__details1">
-                    <p class="lower-message__content">
-                    ${message.content}
-                  </p>
-                  </div>
-                  </div>`
-                return html;
-  };
+        `<div class="contents_main1"data-message-id=${message.id}>
+          <div class="message__sender1">
+            ${message.user_name}
+          </div>
+          <div class="message__date1">
+            ${message.data}
+          </div>
+          <div class="message__details1">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+          </div>`
+        return html;
+    };
   }
 
   $('#new_message').on('submit',function(e){
